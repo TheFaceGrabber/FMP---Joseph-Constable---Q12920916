04f869dc5f0608f37a71e2f08ef1fd499daf5fc4
Joe Constable <joeconstable97@gmail.com>
2020-03-05 14:01:23 +0000

    Added Grid Shader Added Grid Material
    Added Grid Plane rendering
    Grid Plane dependence on aspect ratio
    Grid size depends on user defined grid size (from Grid class)

--- Assets/FMP/Editor/Window.cs
+++ Assets/FMP/Editor/Window.cs
@@ -3,21 +3,29 @@ using UnityEditor;
 
 public class Window : EditorWindow
 {
-enum cameraDirection{
-	Top,
-	Left,
-	Right,
-	Front,
-	Back
-}
+    enum cameraDirection
+    {
+        Top,
+        Left,
+        Right,
+        Front,
+        Back
+    }
 
-	RenderTexture renderTexture;
+
+    /*Window manipulation*/
 	bool showGrid;
 	Vector2 mousePos;
 	Vector2 mouseDragStartPos;
+
+	/*Rendering*/
+    RenderTexture renderTexture;
 	Vector2 lastSize;
 	Camera viewCamera;
 	cameraDirection currentCamDirection;
+
+    private GameObject gridPlane;
+    private Material gridMaterial;
 
     [MenuItem("Tools/FMP/Window")]
     static void Open()
@@ -43,6 +51,8 @@ enum cameraDirection{
 	{
 		DestroyRenderTexture();
 		CreateRenderTexture();
+
+        gridPlane.transform.localScale = new Vector3(position.width / position.height, 1, 1);
 	}
 
 	void Awake()
@@ -50,8 +60,18 @@ enum cameraDirection{
 		GameObject cam = new GameObject("cam");
 		cam.hideFlags = HideFlags.HideAndDontSave | HideFlags.DontSave;
 		viewCamera = cam.AddComponent<Camera>();
+        viewCamera.farClipPlane = 5000;
+        viewCamera.backgroundColor = Color.black;
 		viewCamera.clearFlags = CameraClearFlags.SolidColor;
 		viewCamera.orthographic = true;
+
+        gridPlane = GameObject.CreatePrimitive(PrimitiveType.Plane);
+        gridPlane.transform.parent = viewCamera.transform;
+        gridPlane.transform.localPosition = new Vector3(0, 0, 1000);
+        gridPlane.transform.localEulerAngles = new Vector3(-90,0,0);
+		gridPlane.hideFlags = HideFlags.HideAndDontSave | HideFlags.DontSave;
+		gridPlane.GetComponent<MeshRenderer>().material = Resources.Load<Material>("GridMaterial");
+        gridMaterial = gridPlane.GetComponent<MeshRenderer>().material;
 
 		UpdateViewDirection();
 
@@ -67,34 +87,42 @@ enum cameraDirection{
 		{
 			DestroyImmediate(viewCamera.gameObject);
 		}
+
+		if(gridPlane)
+			DestroyImmediate(gridPlane);
 	}
 
 	void UpdateViewDirection()
 	{
-		switch(currentCamDirection)
-			{
-				case cameraDirection.Top:
-					viewCamera.transform.position = new Vector3(0,1000,0);
-					viewCamera.transform.eulerAngles = new Vector3(90,0,0);
+        switch (currentCamDirection)
+        {
+            case cameraDirection.Top:
+                viewCamera.transform.position = new Vector3(0, 1000, 0);
+                viewCamera.transform.eulerAngles = new Vector3(90, 0, 0);
+                gridMaterial.SetInt("_DrawVertically", 0);
+                break;
+            case cameraDirection.Left:
+                viewCamera.transform.position = new Vector3(1000, 0, 0);
+                viewCamera.transform.eulerAngles = new Vector3(0, -90, 0);
+                gridMaterial.SetInt("_DrawVertically", 1);
 				break;
-				case cameraDirection.Left:
-					viewCamera.transform.position = new Vector3(1000,0,0);
-					viewCamera.transform.eulerAngles = new Vector3(0,-90,0);
+            case cameraDirection.Right:
+                viewCamera.transform.position = new Vector3(-1000, 0, 0);
+                viewCamera.transform.eulerAngles = new Vector3(0, 90, 0);
+                gridMaterial.SetInt("_DrawVertically", 1);
 				break;
-				case cameraDirection.Right:
-					viewCamera.transform.position = new Vector3(-1000,0,0);
-					viewCamera.transform.eulerAngles = new Vector3(0,90,0);
+            case cameraDirection.Front:
+                viewCamera.transform.position = new Vector3(0, 0, -1000);
+                viewCamera.transform.eulerAngles = new Vector3(0, 0, 0);
+                gridMaterial.SetInt("_DrawVertically", 2);
 				break;
-				case cameraDirection.Front:
-					viewCamera.transform.position = new Vector3(0,0,-1000);
-					viewCamera.transform.eulerAngles = new Vector3(0,0,0);
+            case cameraDirection.Back:
+                viewCamera.transform.position = new Vector3(0, 0, 1000);
+                viewCamera.transform.eulerAngles = new Vector3(0, 180, 0);
+                gridMaterial.SetInt("_DrawVertically", 2);
 				break;
-				case cameraDirection.Back:
-					viewCamera.transform.position = new Vector3(0,0,1000);
-					viewCamera.transform.eulerAngles = new Vector3(0,180,0);
-				break;
-			}
-	}
+        }
+    }
 
     void OnGUI()
 	{	//Check for window resizing and update rendertexture accordingly.
@@ -128,10 +156,11 @@ enum cameraDirection{
 			ShowNotification(new GUIContent(currentCamDirection.ToString()));
 		}
 
+		gridPlane.SetActive(true);
 
-		GL.wireframe = true;
+		gridMaterial.SetFloat("_GridSize", Grid.GridSize);
 		viewCamera.Render();
-		GL.wireframe = false;
+		gridPlane.SetActive(false);
 		GUI.DrawTexture(new Rect(0, 0, position.width, position.height), renderTexture);
 	}
 }
---
+++ Assets/FMP/Shaders/GridShader.shader
@@ -0,0 +1,76 @@
+﻿Shader "Unlit/GridShader"
+{
+	Properties
+	{
+		_GridSize("Grid Size", Float) = 1
+		_DrawVertically("Draw Grid Vertically", Int) = 1
+	}
+	SubShader
+	{
+		Tags { "RenderType" = "Overlay" }
+		LOD 100
+		ZTest Always
+
+		Pass
+		{
+			Blend SrcAlpha OneMinusSrcAlpha
+			//Offset -20, -20
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+
+			#include "UnityCG.cginc"
+
+			float _GridSize;
+			int _DrawVertically;
+
+			struct appdata
+			{
+				float4 vertex : POSITION;
+			};
+
+			struct v2f
+			{
+				float2 uv : TEXCOORD0;
+				float4 vertex : SV_POSITION;
+			};
+
+
+			v2f vert(appdata v)
+			{
+				v2f o;
+				o.vertex = UnityObjectToClipPos(v.vertex);
+				if (_DrawVertically == 2)
+					o.uv = mul(unity_ObjectToWorld, v.vertex).yx;
+				else if(_DrawVertically == 1)
+					o.uv = mul(unity_ObjectToWorld, v.vertex).yz;
+				else if(_DrawVertically == 0)
+					o.uv = mul(unity_ObjectToWorld, v.vertex).xz;
+				return o;
+			}
+
+			float DrawGrid(float2 uv, float sz, float aa)
+			{
+				float aaThresh = aa;
+				float aaMin = aa * 0.1;
+
+				float2 gUV = uv / sz + aaThresh;
+
+				float2 fl = floor(gUV);
+				gUV = frac(gUV);
+				gUV -= aaThresh;
+				gUV = smoothstep(aaThresh, aaMin, abs(gUV));
+				float d = max(gUV.x, gUV.y);
+
+				return d;
+			}
+
+			fixed4 frag(v2f i) : SV_Target
+			{
+				fixed grid = DrawGrid(i.uv + float2(0.5,0.5), _GridSize, 0.03);
+				return float4(grid, grid, grid, grid);
+			}
+			ENDCG
+		}
+	}
+}
